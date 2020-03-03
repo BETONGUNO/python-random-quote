84d6a58c7a46cd10982ba48287257045d2a275f2
BETONGUNO <ola.conradson@gmail.com>
2020-03-03 22:02:04 +0100

    encoding=utf-8

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,14 @@ import random
 def primary():
         #last = 13
         #rnd = random.randint(0, last)
-    f = open("C:/users/ola/documents/Python/Gitbud/python-random-quote/quotes.txt")
+    f = open("C:/users/ola/documents/Python/Gitbud/python-random-quote/quotes.txt", encoding="utf-8")
+
     quotes = f.readlines()
     f.close()
     quotesNy = []
     for element in quotes:
           quotesNy.append(element.strip())
-        
+
 
     print(*quotesNy, sep='\n')
-
 primary()
