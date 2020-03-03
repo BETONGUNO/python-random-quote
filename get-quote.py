72323f5f83e9af7962bbfd1a8066f90c2528b9a4
BETONGUNO <ola.conradson@gmail.com>
2020-03-03 19:37:27 +0100

    tagit bort /n

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
 import random
 def primary():
     
-    f = open("quotes.txt")
+    f = open("C:/users/ola/documents/Python/Gitbud/python-random-quote/quotes.txt")
     quotes = f.readlines()
+    quotesNy = []
     f.close()
-    last = 13
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    for element in quotes:
+          quotesNy.append(element.strip())
+    #last = 13
+    #rnd = random.randint(0, last)
+    print(quotesNy)
 
 primary()
