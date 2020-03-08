6435bee1e2a6bd8e669015baa0a32fbef667b6a0
BETONGUNO <ola.conradson@gmail.com>
2020-03-08 17:20:37 +0100

    snyggar till lite

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,10 @@ def primary():
         #last = 13
         #rnd = random.randint(0, last)
     f = open("C:/users/ola/documents/Python/Gitbud/python-random-quote/quotes.txt", encoding="utf-8")  #gör om txt filen till utf 8 så åäö '' syns
-
     quotes = f.readlines()
     f.close()
     quotesNy = []
     for element in quotes:
           quotesNy.append(element.strip())          #Lägger allt på egna rader
-
-
     print(*quotesNy, sep='\n')                      #Tar bort \n
 primary()
